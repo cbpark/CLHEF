478f6f19806ff774910ffe563eb8d3abe8059f80
cbpark <cbpark@gmail.com>
2015-01-27 01:04:28 +0900

    IsInMotherLine -> IsInMotherLines

--- src/lhef.cc
+++ src/lhef.cc
@@ -56,8 +56,7 @@ ParticleLines ParticleLinesOf(const ParticleID& pid, const LHEFEvent& lhe) {
 Particle Mother(const Particle& p, const LHEFEvent& lhe) {
     int mo_line = p.mothup.first;
     auto entry = lhe.particle_entries();
-    auto mo_pos = entry.find(mo_line);
-    if (mo_pos == entry.end()) {  // mother particle not found
+    if (entry.find(mo_line) == entry.end()) {  // mother particle not found
         return entry.at(1);
     } else {
         return entry.at(mo_line);
@@ -76,14 +75,15 @@ Particles Daughters(int pline, const LHEFEvent& lhe) {
     return SelectParticlesBy(pred, lhe);
 }
 
-bool IsInMotherLine(int pline, const Particle& p, const LHEFEvent& lhe) {
-    if (p.mothup.first == 1) {
+bool IsInMotherLines(int pline, const Particle& p, const LHEFEvent& lhe) {
+    int mo_line = p.mothup.first;
+    if (mo_line == 1) {
         return false;
-    } else if (p.mothup.first == pline) {
+    } else if (mo_line == pline) {
         return true;
     } else {
         Particle direct_mother = Mother(p, lhe);
-        return IsInMotherLine(pline, direct_mother, lhe);
+        return IsInMotherLines(pline, direct_mother, lhe);
     }
 }
 
@@ -91,7 +91,7 @@ Particles FinalDaughters(int pline, const LHEFEvent& lhe) {
     Particles finalstates = FinalStates(lhe);
     auto pos = std::remove_if(finalstates.begin(), finalstates.end(),
                               [&] (const Particle& p) {
-                                  return !IsInMotherLine(pline, p, lhe);
+                                  return !IsInMotherLines(pline, p, lhe);
                               });
     finalstates.erase(pos, finalstates.end());
     return finalstates;
